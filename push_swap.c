2ae97df142933330868eded8c6c2e723a85f6c68
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-07-30 09:52:29 +0200

    pp

--- push_swap.c
+++ push_swap.c
@@ -6,80 +6,64 @@
 /*   By: atitus <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/23 12:12:50 by atitus            #+#    #+#             */
-/*   Updated: 2019/07/26 11:02:10 by atitus           ###   ########.fr       */
+/*   Updated: 2019/07/30 09:52:12 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void 	sort_3(t_stack *head)
+void 	sort_3(t_stack **head)
 {
-	t_stack *temp;
+	t_stack **temp;
 
-	temp = head;
-	if(temp->val > temp->next->val && temp->next->val < temp->next->next->val)
+	*temp = *head;
+	if((*temp)->val > (*temp)->next->val && (*temp)->next->val < (*temp)->next->next->val)
 		ft_putendl("sa");
-	else if (temp->val > temp->next->val && temp->next->val > temp->next->next->val)
+	else if ((*temp)->val > (*temp)->next->val && (*temp)->next->val > (*temp)->next->next->val)
 	{
 		ft_putendl("sa");
 		ft_putendl("rra");
 	}
-	else if (temp->val > temp->next->val && temp->next->val < temp->next->next->val)
+	else if ((*temp)->val > (*temp)->next->val && (*temp)->next->val < (*temp)->next->next->val)
 		ft_putendl("ra");
-	else if (temp->val < temp->next->val && temp->next->val > temp->next->next->val)
+	else if ((*temp)->val < (*temp)->next->val && (*temp)->next->val > (*temp)->next->next->val)
 	{
 		ft_putendl("sa");
 		ft_putendl("ra");
 	}
-	else if (temp->val < temp->next->val && temp->next->val > temp->next->next->val)
+	else if ((*temp)->val < (*temp)->next->val && (*temp)->next->val > (*temp)->next->next->val)
 		ft_putendl("rra");
 }
 
-
-
-/*void 	sort_5(t_stack *head)
+void	sort_5(t_stack **head)
 {
-	t_stack *temp;
-	
-	ft_putendl("pb");
-	ft_putendl("pb");
-	temp = head;
-	if(temp->val > temp->next->val && temp->next->val < temp->next->next->val)
-		ft_putendl("sa");
-	else if (temp->val > temp->next->val && temp->next->val > temp->next->next->val)
+	t_stack **temp;
+	int minimum;
+
+	*temp = *head;
+	minimum = 0;
+	minimum = min(*temp);
+
+	if(minimum == (*temp)->val)
+		ft_putendl("pb");
+	else if (minimum == (*temp->next->val))
 	{
 		ft_putendl("sa");
-		ft_putendl("rra");
-	}
-	else if (temp->val > temp->next->val && temp->next->val < temp->next->next->val)
-		ft_putendl("ra");
-	else if (temp->val < temp->next->val && temp->next->val > temp->next->next->val)
+		ft_putendl("pb");
+	}else if(minimum == (*temp)->next->next->val)
 	{
-		ft_putendl("sa");
 		ft_putendl("ra");
-	}
-	else if (temp->val < temp->next->val && temp->next->val > temp->next->next->val)
+		ft_putendl("sa");
+		ft_putendl("pb");
+	}else if(minimum == (*temp)->next->next->next->val)
+	{
 		ft_putendl("rra");
+		ft_putendl("rra");
+		ft_putendl("pb");
+	}else
+	{
+		ft_putendl("rra");
+		ft_putendl("pb");
+	}
 }
-*/
 
-
-int		is_sorted(t_stack *head)
-{
-	t_stack *nodePtr;
-    nodePtr = head;
-    int i;
-
-    if (!nodePtr)
-    	return 0; // Empty list
-    i = nodePtr->val;
-    nodePtr = nodePtr->next;
-    while (nodePtr)
-    {
-        if(i > nodePtr->val)
-        	return -1;
-        i = nodePtr->val;
-        nodePtr = nodePtr->next;
-    }
-    return 1;
-}
