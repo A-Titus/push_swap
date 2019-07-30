f22cbc8b8391840232e350dc28248a6d962606be
Abdussamad TITUS <atitus@c4r10s2.wethinkcode.co.za>
2019-07-30 09:41:16 +0200

    changes

--- main.c
+++ main.c
@@ -2,6 +2,17 @@
 
 int main(int argc, char **argv)
 {
-	add_node(argc, argv);
+	t_stack *a;
+
+	a = add_node(argc, argv);
+	if(list_size(a) == 2)
+	{
+		if(is_sorted(a) != 1)
+			ft_putendl("sa");
+	}else if (list_size(a) == 3)
+	{
+		if(is_sorted(a) != 1)
+			sort_3(&a);
+	}
 	return (0);
 }
