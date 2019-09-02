a8d2544eb2c2c3e51f32dedbcdf2ba9a0e3d1c23
Abdussamad TITUS <atitus@c4r10s5.wethinkcode.co.za>
2019-09-02 15:48:52 +0200

    aaaaaaa

--- push_sorted.c
+++ push_sorted.c
@@ -6,16 +6,37 @@
 /*   By: atitus <atitus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 11:36:50 by atitus            #+#    #+#             */
-/*   Updated: 2019/09/02 12:37:07 by atitus           ###   ########.fr       */
+/*   Updated: 2019/09/02 15:46:58 by atitus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int     middle(t_stack *head)  
+{  
+    t_stack *slow_ptr = head;  
+    t_stack *fast_ptr = head;  
+    int i = 0;
+  
+    if (head!=NULL)  
+    {  
+        while (fast_ptr != NULL && fast_ptr->next != NULL)  
+        {  
+            fast_ptr = fast_ptr->next->next;  
+            slow_ptr = slow_ptr->next;
+            i++;  
+        }  
+       
+    }  
+    return(i);
+}
+
 void    push_sorted(t_stack **b)
 {
+    
     int size;
     int i;
+    int middle_val = middle(*b);
     t_stack *current = NULL;
     
     size = list_size(*b);
@@ -37,12 +58,19 @@ void    push_sorted(t_stack **b)
 			pa(&a, b, (*b)->val);
 			ft_putendl("pa");
 		}
-		else
+		else (current->val != max_val) //&& i <= middle_val)
 		{
 			
-            rrb(b);
+            rb(b, 1);
 			ft_putendl("rb");
 		}
+       /* else if (current->val != max_val && i > middle_val)
+        {
+            rrb(b);
+            ft_putendl("rrb");
+        }*/
         i++;
     }
-}
+
+    
+}  
